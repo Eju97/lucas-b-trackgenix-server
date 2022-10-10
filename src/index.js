6e5217bba777f6937667f55ad8dc02cf0fd60f10
anapaulib <anapaulib@gmail.com>
2022-10-10 09:54:27 -0300

    TG: getTaskList completed, corrections made in getTaskById and DeleteTaskById

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // use "import" to import libraries
 import express from 'express';
 import getAllAdmins from './resources/admins';
-import { getTaskById, deleteTaskById } from './resources/tasks';
+import { getTaskById, deleteTaskById, getTaskList } from './resources/tasks';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -17,6 +17,8 @@ app.get('/admins', getAllAdmins);
 app.get('/getTaskById/:id', getTaskById);
 
 app.delete('/deleteTaskById/:id', deleteTaskById);
+
+app.get('/getTaskList', getTaskList);
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
--- src/resources/tasks.js
+++ src/resources/tasks.js
@@ -5,9 +5,13 @@ export const getTaskById = (req, res) => {
   const taskId = parseInt(req.params.id, 10);
   const foundTask = tasks.find((task) => task.id === taskId);
   if (foundTask) {
-    res.send(foundTask);
+    res.status(200).json({
+      data: foundTask,
+    });
   } else {
-    res.send('Task not found');
+    res.status(404).json({
+      message: 'Task not found',
+    });
   }
 };
 
@@ -16,12 +20,27 @@ export const deleteTaskById = (req, res) => {
   const deletedTask = tasks.filter((task) => task.id !== taskId);
   if (deletedTask.length < tasks.length) {
     fs.writeFileSync('src/data/tasks.json', JSON.stringify(deletedTask));
-    res.send('Task has been deleted');
+    res.status(200).json({
+      message: 'Task has been deleted',
+    });
   } else {
-    res.send('Task could not be deleted');
+    res.status(404).json({
+      message: 'Task could not be deleted',
+    });
   }
 };
 
-/* export const getTaksList = (req, res) => {
-    const taskName = req.
-} */
+export const getTaskList = (req, res) => {
+  const taskName = req.query.name;
+  const taskDescription = req.query.description;
+  let filteredList = tasks;
+  if (taskName) {
+    filteredList = filteredList.filter((task) => task.name.includes(taskName));
+  }
+  if (taskDescription) {
+    filteredList = filteredList.filter((task) => task.description.includes(taskDescription));
+  }
+  res.status(200).json({
+    data: filteredList,
+  });
+};
