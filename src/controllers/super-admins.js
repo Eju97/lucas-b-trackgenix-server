dc6e8aead5ea198486aad2d2952cf464b13bf4a8
Martin Garcia <martin-garcia95@hotmail.com>
2022-10-14 18:03:41 -0300

    TG-9: Fixed errors from the PR

--- src/controllers/super-admins.js
+++ src/controllers/super-admins.js
@@ -3,20 +3,17 @@ import SuperAdmins from '../models/Super-admins';
 const editSuperAdmins = async (req, res) => {
   try {
     const { id } = req.params;
-    const result = await SuperAdmins.findByIdAndUpdate(
-      { _id: id },
-      { ...req.body },
-      { new: true },
-    );
+    const result = await SuperAdmins.findByInAndUpdate(id, req.body, { new: true });
     return res.status(200).json({
       message: `Super Admin with ID ${id} edited.`,
       data: result,
       error: false,
     });
   } catch (error) {
-    return res.json({
-      message: 'An error occurred',
-      error,
+    return res.status(400).json({
+      message: error,
+      data: undefined,
+      error: true,
     });
   }
 };
@@ -32,9 +29,10 @@ const deletedSuperAdmins = async (req, res) => {
       error: false,
     });
   } catch (error) {
-    return res.json({
-      message: 'An error occurred',
-      error,
+    return res.status(400).json({
+      message: error,
+      data: undefined,
+      error: true,
     });
   }
 };
