7c99f8352161e15557dd0e7ab26b67c6a0ae6b59
DamianPalavecino <damianpalavecino25@gmail.com>
2022-10-15 20:19:51 -0300

    TG-6: Create new middleware to validate new project creation

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { getProjects, getProjectById, createProjects } from '../controllers/projects';
+import validateProjectBody from '../validations/projects';
 
 const router = express.Router();
 
 router
   .get('/', getProjects)
   .get('/:id', getProjectById)
-  .post('/', createProjects);
+  .post('/', validateProjectBody, createProjects);
 
 export default router;
