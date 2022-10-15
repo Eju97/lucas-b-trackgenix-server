d1fe5600272cd55936edcf63ccb6195bab25f8be
Leandro Di Simone <disimoneleandro@gmail.com>
2022-10-15 13:18:49 -0300

    TG-13: created validation from timesheets

--- src/routes/time-sheets.js
+++ src/routes/time-sheets.js
@@ -2,13 +2,18 @@ import express from 'express';
 import {
   getAllTimeSheets, editTimeSheet, deleteTimeSheet,
 } from '../controllers/time-sheets';
-// import timeSheetsValidations from '../validations/time-sheets';
+import validateCreation from '../validations/time-sheets';
 
 const router = express.Router();
 
+// const Logger = (req, res, next) => {
+//   console.log('middleware running');
+//   return next();
+// };
+
 router
   .get('/', getAllTimeSheets)
-  .put('/:id', editTimeSheet)
+  .put('/:id', validateCreation, editTimeSheet)
   .delete('/:id', deleteTimeSheet);
 
 export default router;
