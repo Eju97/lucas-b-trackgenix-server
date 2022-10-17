fd27f0218bd0b1dbe79f4760e0fb2128bc44a6f5
anapaulib <anapaulib@gmail.com>
2022-10-17 11:00:56 -0300

    TG-12: joi validations added to createTimeSheet endpoint

--- src/routes/time-sheets.js
+++ src/routes/time-sheets.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { getAllTimeSheets, getTimeSheetById, createTimeSheet } from '../controllers/time-sheets';
-// import validateTimeSheet from '../validations/time-sheets';
+import validateTimeSheet from '../validations/time-sheets';
 
 const router = express.Router();
 
 router
   .get('/', getAllTimeSheets)
   .get('/:id', getTimeSheetById)
-  .post('/', createTimeSheet);
+  .post('/', validateTimeSheet, createTimeSheet);
 export default router;
