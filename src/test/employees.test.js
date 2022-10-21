885677ae1b9b612de23909a31b79797f27ee40cb
Gonzalo Asencio <gestorgonzalo@gmail.com>
2022-10-21 18:06:27 -0300

    TG-20:Add Employee seed and Get test

--- src/test/employees.test.js
+++ src/test/employees.test.js
@@ -1,3 +1,17 @@
-test('', () => {
-  expect(2).toBe(2);
+import request from 'supertest';
+import app from '../app';
+import Employees from '../models/Employees';
+import employeesSeed from '../seed/employees';
+
+beforeAll(async () => {
+  await Employees.collection.insertMany(employeesSeed);
+});
+
+describe('GET /employees', () => {
+  test('should get code 200', async () => {
+    const response = await request(app).get('/Employees').send();
+    expect(response.status).toBe(200);
+    expect(response.body.error).toBeFalsy();
+    expect(response.body.data.length).toBeGreaterThan(0);
+  });
 });
