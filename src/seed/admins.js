a77df7172ba0a2885cc0b3caceff57ff9c035e6d
Elias Martinez <martinezelias17@gmail.com>
2022-10-22 14:34:27 -0300

    TG-15: create seed for admins

---
+++ src/seed/admins.js
@@ -0,0 +1,19 @@
+export default [{
+    _id: mongoose.Types.ObjectId('635427a844b3ae1fa298b813'),
+    name: 'Elias',
+    lastName: 'Martinez',
+    email: 'radium@rocket.com',
+    password: 'radium123',
+    dni: '34234879',
+    phone: '5493416333987',
+  },
+  {
+    _id: mongoose.Types.ObjectId('6354282c9f5bfbedd3b363c7'),
+    name: 'Radium',
+    lastName: 'Rocket',
+    email: 'radium@rocket.com',
+    password: 'radium789',
+    dni: '34908658',
+    phone: '5493416785464',
+  }
+]
