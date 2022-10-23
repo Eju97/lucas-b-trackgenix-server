cc2792d4f10b283aa2b810ddd57c3924b2bd652a
Elias Martinez <martinezelias17@gmail.com>
2022-10-23 18:47:48 -0300

    TG-15: create test with get method

--- src/seed/admins.js
+++ src/seed/admins.js
@@ -1,19 +1,31 @@
+import mongoose from 'mongoose';
+
 export default [{
-    _id: mongoose.Types.ObjectId('635427a844b3ae1fa298b813'),
-    name: 'Elias',
-    lastName: 'Martinez',
-    email: 'radium@rocket.com',
-    password: 'radium123',
-    dni: '34234879',
-    phone: '5493416333987',
-  },
-  {
-    _id: mongoose.Types.ObjectId('6354282c9f5bfbedd3b363c7'),
-    name: 'Radium',
-    lastName: 'Rocket',
-    email: 'radium@rocket.com',
-    password: 'radium789',
-    dni: '34908658',
-    phone: '5493416785464',
-  }
-]
+  _id: mongoose.Types.ObjectId('635427a844b3ae1fa298b813'),
+  name: 'Lenovo',
+  lastName: 'Asdfgh',
+  email: 'lenovo@asdfgh.ar',
+  password: 'Radium123',
+},
+{
+  _id: mongoose.Types.ObjectId('6354282c9f5bfbedd3b363c7'),
+  name: 'Radium',
+  lastName: 'Rocket',
+  email: 'radium@rocket.com',
+  password: 'Radium789',
+},
+{
+  _id: mongoose.Types.ObjectId('6354ac2b005533b9588a0ec9'),
+  name: 'John',
+  lastName: 'Trackgenix',
+  email: 'asdasd@rocasd.ar',
+  password: 'Track123234',
+},
+{
+  _id: mongoose.Types.ObjectId('6354ac37efc9ff2e99780d92'),
+  name: 'Radio',
+  lastName: 'Escucha',
+  email: 'radio@escucha.com',
+  password: 'Escucha1234',
+},
+];
