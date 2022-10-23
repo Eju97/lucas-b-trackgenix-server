14c0c1777a6ca3da1617823ef5b9b77419e957cb
Elias Martinez <martinezelias17@gmail.com>
2022-10-23 18:49:58 -0300

    TG-15: create test with get method

--- src/test/admins.test.js
+++ src/test/admins.test.js
@@ -7,17 +7,17 @@ describe('Admins - Unit tests', () => {
   beforeAll(async () => {
     await Admins.collection.insertMany(adminsSeed);
   });
-});
 
-describe('GET /admins', () => {
-  test('Should return status code 404 when the path is wrong.', async () => {
-    const res = await request(app).get('/admin').send();
-    expect(res.status).toBe(404);
-  });
-  test('Should return status code 200 and the admins.', async () => {
-    const res = await request(app).get('/admins').send();
-    expect(res.status).toBe(200);
-    expect(res.body.error).toBeFalsy();
-    expect(res.body.data.length).toBeGreaterThan(0);
+  describe('GET /admins', () => {
+    test('Should return status code 404 when the path is wrong.', async () => {
+      const res = await request(app).get('/admin').send();
+      expect(res.status).toBe(404);
+    });
+    test('Should return status code 200 and the admins.', async () => {
+      const res = await request(app).get('/admins').send();
+      expect(res.status).toBe(200);
+      expect(res.body.error).toBeFalsy();
+      expect(res.body.data.length).toBeGreaterThan(0);
+    });
   });
 });
