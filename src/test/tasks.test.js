4267829019c5b21094064707eae80bfe13250bcd
anapaulib <anapaulib@gmail.com>
2022-10-24 21:28:47 -0300

    TG-19: tasks seed and tests corrected

--- src/test/tasks.test.js
+++ src/test/tasks.test.js
@@ -3,7 +3,7 @@ import app from '../app';
 import Tasks from '../models/Tasks';
 import tasks from '../seed/tasks';
 
-beforeAll (async() => {
+beforeAll(async () => {
   await Tasks.collection.insertMany(tasks);
 });
 
@@ -16,7 +16,7 @@ const mockedTask = {
 };
 
 describe('GET /tasks', () => {
-  test ('Should return the list of tasks', async() => {
+  test('Should return the list of tasks', async () => {
     const res = await request(app).get('/tasks').send();
     expect(res.status).toBe(200);
     expect(res.body.error).toBeFalsy();
@@ -26,7 +26,7 @@ describe('GET /tasks', () => {
 });
 
 describe('GET /tasks/:id', () => {
-  test ('Should get a task', async() => {
+  test('Should get a task', async () => {
     const res = await request(app).get(`/tasks/${correctMockedTaskId}`).send();
     expect(res.status).toBe(200);
     expect(res.body.error).toBeFalsy();
@@ -36,13 +36,13 @@ describe('GET /tasks/:id', () => {
       _id: correctMockedTaskId,
     });
   });
-  test ('Should return status code 404 when you send a non existing id', async() => {
+  test('Should return status code 404 when you send a non existing id', async () => {
     const res = await request(app).get(`/tasks/${wrongMockedTaskId}`).send();
     expect(res.status).toBe(404);
     expect(res.body.error).toBeFalsy();
     expect(res.body.message).toBe('Task does not exists');
   });
-  test ('Should return status code 400 when you send an invalid id', async() => {
+  test('Should return status code 400 when you send an invalid id', async () => {
     const res = await request(app).get(`/tasks/${wrongTaskId}`).send();
     expect(res.status).toBe(400);
     expect(res.body.error).toBeTruthy();
@@ -50,19 +50,19 @@ describe('GET /tasks/:id', () => {
 });
 
 describe('DELETE /tasks/:id', () => {
-  test ('Should delete a task', async() => {
+  test('Should delete a task', async () => {
     const res = await request(app).delete(`/tasks/${correctMockedTaskId}`).send();
     expect(res.status).toBe(200);
     expect(res.body.error).toBeFalsy();
     expect(res.body.message).toBe(`Task with Id ${correctMockedTaskId} deleted`);
   });
-  test ('Should return status code 404 when you send a non existing id', async() => {
+  test('Should return status code 404 when you send a non existing id', async () => {
     const res = await request(app).delete(`/tasks/${wrongMockedTaskId}`).send();
     expect(res.status).toBe(404);
     expect(res.body.error).toBeFalsy();
     expect(res.body.message).toBe(`Task with Id ${wrongMockedTaskId} does not exists`);
   });
-  test ('Should return status code 400 when you send an invalid id', async() => {
+  test('Should return status code 400 when you send an invalid id', async () => {
     const res = await request(app).delete(`/tasks/${wrongTaskId}`).send();
     expect(res.status).toBe(400);
     expect(res.body.error).toBeTruthy();
@@ -70,13 +70,13 @@ describe('DELETE /tasks/:id', () => {
 });
 
 describe('POST /tasks', () => {
-  test ('Should create a task', async() => {
+  test('Should create a task', async () => {
     const res = await request(app).post('/tasks').send(mockedTask);
     expect(res.status).toBe(201);
     expect(res.body.error).toBeFalsy();
     expect(res.body.message).toBe('Task created successfully');
   });
-  test ('Should return status code 400 when the task is not created', async() => {
+  test('Should return status code 400 when the task is not created', async () => {
     const res = await request(app).post('/tasks').send();
     expect(res.status).toBe(400);
     expect(res.body.error).toBeTruthy();
@@ -84,24 +84,27 @@ describe('POST /tasks', () => {
 });
 
 describe('PUT /tasks/:id', () => {
-  test ('Should edit a task', async() => {
+  test('Should edit a task', async () => {
+    // eslint-disable-next-line no-underscore-dangle
     const res = await request(app).put(`/tasks/${tasks[0]._id}`).send(mockedTask);
     expect(res.status).toBe(200);
     expect(res.body.error).toBeFalsy();
     expect(res.body.data).toBeDefined();
+    // eslint-disable-next-line no-underscore-dangle
     expect(res.body.message).toBe(`Task with Id ${tasks[0]._id} updated`);
     expect(res.body.data).toMatchObject({
-       _id: tasks[0]._id,
-       description: mockedTask.description,
-   });
+      // eslint-disable-next-line no-underscore-dangle
+      _id: tasks[0]._id,
+      description: mockedTask.description,
+    });
   });
-  test ('Should return status code 404 when you send a non existing id', async() => {
+  test('Should return status code 404 when you send a non existing id', async () => {
     const res = await request(app).put(`/tasks/${wrongMockedTaskId}`).send(mockedTask);
     expect(res.status).toBe(404);
     expect(res.body.error).toBeFalsy();
     expect(res.body.message).toBe(`Task with Id ${wrongMockedTaskId} does not exists`);
   });
-  test ('Should return status code 400 when you send an invalid id', async() => {
+  test('Should return status code 400 when you send an invalid id', async () => {
     const res = await request(app).put(`/tasks/${wrongTaskId}`).send(mockedTask);
     expect(res.status).toBe(400);
     expect(res.body.error).toBeTruthy();
