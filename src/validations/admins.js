00ab484e2dbb7bbe212fbb80313637b76be5a1aa
Ernesto Bessone <ejubessone@gmail.com>
2022-11-16 19:58:50 -0300

    TG-51: added validation error messages to admin entitie

--- src/validations/admins.js
+++ src/validations/admins.js
@@ -1,16 +1,48 @@
 import Joi from 'joi';
 
 export const validateCreation = (req, res, next) => {
-  const letterSpacesRegEx = /^[a-zA-Z\s]*$/;
+  // const letterSpacesRegEx = /^[a-zA-Z\s]*$/;
+  const letterSpacesRegEx = /[A-Za-z]{3}([A-Za-z]+ ?)*/;
   const adminValidation = Joi.object({
-    name: Joi.string().min(3).max(50).regex(letterSpacesRegEx)
-      .required(),
-    lastName: Joi.string().min(3).max(50).regex(letterSpacesRegEx)
-      .required(),
-    email: Joi.string().email()
-      .required(),
-    password: Joi.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/)
-      .required(),
+    name: Joi.string()
+      .min(3)
+      .trim()
+      .max(30)
+      .regex(letterSpacesRegEx)
+      .required()
+      .messages({
+        'any.required': 'Name is required',
+        'string.base': 'Name must be a string',
+        'string.empty': 'Name is not allowed to be empty',
+        'string.min': 'Name must have a minimum of 3 letters',
+        'string.max': 'Name can contain more than 30 letters',
+        'string.pattern.base': 'Name must have a minimum of 3 letters',
+        'string.required': 'Name field is required',
+      }),
+    lastName: Joi.string().min(3).max(30).regex(letterSpacesRegEx)
+      .required()
+      .messages({
+        'any.required': 'Last Name is required',
+        'string.empty': 'Last Name is not allowed to be empty',
+        'string.base': 'Last Name must be a string',
+        'string.min': 'Last Name must have a minimum of 3 letters',
+        'string.max': 'Last Name can contain more than 30 letters',
+        'string.pattern.base': 'Last Name must have a minimum of 3 letters',
+        'string.required': 'Last Name field is required',
+      }),
+    email: Joi.string().email().required().messages({
+      'any.required': 'an email is required',
+      'string.email': 'Insert a valid email',
+      'string.empty': 'Email is not allowed to be empty',
+      'string.required': 'email field is required',
+    }),
+    password: Joi.string()
+      .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/)
+      .required().messages({
+        'any.required': 'a password is required',
+        'string.pattern.base': 'Password must contain at least 8 characters, "one" capital letter, "one" lower case and "one" number at least',
+        'string.required': 'email field is required',
+      }),
   });
   const validation = adminValidation.validate(req.body);
   if (validation.error) {
@@ -26,7 +58,10 @@ export const validateCreation = (req, res, next) => {
 const checkQueryParams = (obj) => {
   const validQueryParams = ['name', 'lastName', 'email', 'password'];
   const keyList = Object.keys(obj);
-  return keyList.length === 0 || keyList.every((key) => (validQueryParams.includes(key)));
+  return (
+    keyList.length === 0
+    || keyList.every((key) => validQueryParams.includes(key))
+  );
 };
 
 export const validateQueryParams = (req, res, next) => {
@@ -42,12 +77,41 @@ export const validateQueryParams = (req, res, next) => {
 };
 
 export const validateEdit = (req, res, next) => {
-  const letterSpacesRegEx = /^[a-zA-Z\s]*$/;
+  const letterSpacesRegEx = /[A-Za-z]{3}([A-Za-z]+ ?)*/;
   const adminValidation = Joi.object({
-    name: Joi.string().min(3).max(50).regex(letterSpacesRegEx),
-    lastName: Joi.string().min(3).max(50).regex(letterSpacesRegEx),
-    email: Joi.string().email(),
-    password: Joi.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/),
+    name: Joi.string().min(3).max(30).regex(letterSpacesRegEx)
+      .messages({
+        'any.required': 'Name is required',
+        'string.base': 'Name must be a string',
+        'string.empty': 'Name is not allowed to be empty',
+        'string.min': 'Name must have a minimum of 3 letters',
+        'string.max': 'Name can contain more than 30 letters',
+        'string.pattern.base': 'Name must have a minimum of 3 letters',
+        'string.required': 'Name field is required',
+      }),
+    lastName: Joi.string().min(3).max(30).regex(letterSpacesRegEx)
+      .messages({
+        'any.required': 'Last Name is required',
+        'string.empty': 'Last Name is not allowed to be empty',
+        'string.base': 'Last Name must be a string',
+        'string.min': 'Last Name must have a minimum of 3 letters',
+        'string.max': 'Last Name can contain more than 30 letters',
+        'string.pattern.base': 'Last Name must have a minimum of 3 letters',
+        'string.required': 'Last Name field is required',
+      }),
+    email: Joi.string().email().messages({
+      'any.required': 'an email is required',
+      'string.email': 'Insert a valid email',
+      'string.empty': 'Email is not allowed to be empty',
+      'string.required': 'email field is required',
+    }),
+    password: Joi.string().regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
+    ).messages({
+      'any.required': 'a password is required',
+      'string.pattern.base': 'Password must contain at least 8 characters, "one" capital letter, "one" lower case and "one" number at least',
+      'string.required': 'email field is required',
+    }),
   });
   const validation = adminValidation.validate(req.body);
   if (validation.error) {
