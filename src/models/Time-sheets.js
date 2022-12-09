0448a61606833c1ef620c019da7e02e131421fe4
AlejoFGM <alejo.fgm@gmail.com>
2022-12-09 14:53:42 -0300

    TG-138: modify delete function in project, task and timesheet

--- src/models/Time-sheets.js
+++ src/models/Time-sheets.js
@@ -9,6 +9,7 @@ const timeSheetsSchema = new Schema({
   task: { type: Schema.Types.ObjectId, ref: 'Tasks' },
   employee: { type: Schema.Types.ObjectId, ref: 'Employees' },
   project: { type: Schema.Types.ObjectId, ref: 'Projects' },
+  isDeleted: { type: Boolean, default: false },
 });
 
 export default mongoose.model('TimeSheets', timeSheetsSchema);
