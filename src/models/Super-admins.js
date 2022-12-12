7a6f20212fc99ac00e0a1e96f8921e2873902404
Luciano Franchino <luciano_franchino@hotmail.com>
2022-12-12 14:49:32 -0300

    TG-137: add isDeleted to superAdmin schema in models

--- src/models/Super-admins.js
+++ src/models/Super-admins.js
@@ -6,6 +6,7 @@ const superAdminsSchema = new Schema({
   last_name: { type: String, require: true },
   email: { type: String, require: true },
   firebaseUid: { type: String, required: true },
+  isDeleted: { type: String, default: false },
 });
 
 export default mongoose.model('SuperAdmins', superAdminsSchema);
