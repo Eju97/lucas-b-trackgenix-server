d9e0c4d700b174086a188814afc2df7c46a8d7d0
Ernesto Bessone <ejubessone@gmail.com>
2022-12-16 23:42:52 -0300

    TG-131:  validation updated

--- src/validations/projects.js
+++ src/validations/projects.js
@@ -4,24 +4,58 @@ export const validateProjectBody = (req, res, next) => {
   const projectSchema = Joi.object({
     name: Joi.string()
       .min(3)
-      .max(50)
+      .max(20)
       .regex(/^[a-zA-Z0-9 ]*$/)
-      .required(),
+      .required()
+      .messages({
+        'any.required': 'Project Name is required',
+        'string.empty': 'Project Name is not allowed to be empty',
+        'string.min': 'Project Name must have a minimum of 3 characters',
+        'string.max': 'Project Name can not be longer than 20 characters',
+        'string.pattern.base': 'Project Name can only contain letters',
+        'string.required': 'Project Name field is required',
+      }),
     clientName: Joi.string()
       .min(3)
-      .max(50)
+      .max(20)
       .regex(/^[a-zA-Z0-9 ]*$/)
-      .required(),
+      .required()
+      .messages({
+        'any.required': 'Client Name is required',
+        'string.empty': 'Client Name is not allowed to be empty',
+        'string.min': 'Client Name must have a minimum of 3 characters',
+        'string.max': 'Client Name can not be longer than 20 characters',
+        'string.pattern.base': 'Client Name can only contain letters',
+        'string.required': 'Client Name field is required',
+      }),
     description: Joi.string()
       .min(3)
-      .max(300)
-      .required(),
+      .max(100)
+      .required()
+      .messages({
+        'any.required': 'Description is required',
+        'string.empty': 'Description is not allowed to be empty',
+        'string.min': 'Description must have a minimum of 3 characters',
+        'string.max': 'Description can not be longer than 100 characters',
+        'string.pattern.base': 'Description can only contain letters',
+        'string.required': 'Description field is required',
+      }),
     startDate: Joi.date()
       .iso()
-      .required(),
+      .required()
+      .messages({
+        'data.empty': 'Start Date is not allowed to be empty',
+        'date.format': 'Start Date must follow the pattern DD-MM-AAAA',
+        'date.required': 'Start Date is required',
+      }),
     endDate: Joi.date()
       .iso()
-      .required(),
+      .required()
+      .messages({
+        'data.empty': 'End Date is not allowed to be empty',
+        'date.format': 'End Date must follow the pattern DD-MM-AAAA',
+        'date.required': 'End Date is required',
+      }),
     employees: Joi.array().items.required(Joi.object({
       employee: Joi.string().hex().length(24).required(),
       rate: Joi.number().required(),
